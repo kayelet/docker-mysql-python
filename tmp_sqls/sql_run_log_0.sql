d19a58c69fcd87d5dd7f8283ec18075ff451d668
kayelet <80198055+kayelet@users.noreply.github.com>
2022-01-23 11:12:59 +0200

    Delete tmp_sqls directory

--- tmp_sqls/sql_run_log_0.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS sql_run_log (
-  run_id int NOT NULL,
-  sql_name varchar(100) NOT NULL,
-  run_status varchar(20) NOT NULL,
-  error_message varchar(255),
-  date datetime NOT NULL,
-  PRIMARY KEY (run_id, sql_name, run_status)
-)
-ENGINE=INNODB  DEFAULT CHARSET=utf8 ;
